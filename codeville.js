50f46f034dc83968d7e5ed0c88d0494653f56bd0
Natalia Martian <natalia.martian@gmail.com>
2020-10-07 14:20:33 +0000

    Function that extracts appropriate voting stations from an array of stations

--- codeville.js
+++ codeville.js
@@ -1,3 +1,7 @@
+/*Your function will receive an array of stations, where each station itself is an array with a name, a capacity, and a venue type.
+
+In order for a station to be deemed appropriate, it must have a capacity of at least 20, and be a school or community centre.*/
+
 const stations = [
   ['Big Bear Donair', 10, 'restaurant'],
   ['Bright Lights Elementary', 50, 'school'],
