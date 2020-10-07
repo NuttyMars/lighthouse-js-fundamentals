f96f7dfce0f7deaf7d9bec62a8a62e75de04e476
Natalia Martian <natalia.martian@gmail.com>
2020-10-07 14:18:46 +0000

    Function that extracts appropriate voting stations from array of stations

---
+++ codeville.js
@@ -0,0 +1,16 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+const chooseStations = function(stations) {
+  let votingCenters = [];
+  for (let i = 0; i < stations.length; i++) {
+    if ((stations[i][1] >= 20) && (stations[i][2] === 'school' || stations[i][2] === 'community centre')) {
+      votingCenters.push(stations[i][0]);
+    }
+  }
+  return votingCenters;
+};
+console.log(chooseStations(stations));
